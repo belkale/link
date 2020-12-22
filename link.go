463ff4d8930d8e4d081bc6ffea5a97c4469f468b
Naveen Belkale <bnaveenbhat@gmail.com>
2020-12-22 15:16:01 +0530

    Solution to the exercise

---
+++ link.go
@@ -0,0 +1,70 @@
+package link
+
+import (
+	"io"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+type Link struct {
+	HREF string
+	Text string
+}
+
+func removeEmpty(ss []string) []string {
+	r := make([]string, 0, len(ss))
+	for _, s := range ss {
+		if strings.TrimSpace(s) == "" {
+			continue
+		}
+
+		r = append(r, s)
+	}
+	return r
+}
+func getText(n *html.Node) string {
+	var data []string
+	switch n.Type {
+	case html.ElementNode:
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			data = append(data, getText(c))
+		}
+	case html.TextNode:
+		data = append(data, strings.TrimSpace(n.Data))
+	}
+	return strings.Join(removeEmpty(data), " ")
+}
+
+func getHREF(attrs []html.Attribute) string {
+	for _, a := range attrs {
+		if a.Key == "href" {
+			return strings.TrimSpace(a.Val)
+		}
+	}
+	return ""
+}
+
+func Parse(r io.Reader) ([]*Link, error) {
+	doc, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var links []*Link
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			l := &Link{
+				HREF: getHREF(n.Attr),
+				Text: getText(n),
+			}
+			links = append(links, l)
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	return links, nil
+}
---
+++ link_test.go
@@ -0,0 +1,85 @@
+package link
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParse(t *testing.T) {
+	tests := []struct {
+		desc string
+		name string
+		want []*Link
+	}{
+		{
+			desc: "ex1.html",
+			name: "ex1.html",
+			want: []*Link{
+				&Link{HREF: "/other-page", Text: "A link to another page"},
+			},
+		},
+		{
+			desc: "ex2.html",
+			name: "ex2.html",
+			want: []*Link{
+				&Link{
+					HREF: "https://www.twitter.com/joncalhoun",
+					Text: "Check me out on twitter",
+				},
+				&Link{
+					HREF: "https://github.com/gophercises",
+					Text: "Gophercises is on Github !",
+				},
+			},
+		},
+		{
+			desc: "ex3.html",
+			name: "ex3.html",
+			want: []*Link{
+				&Link{
+					HREF: "#",
+					Text: "Login",
+				},
+				&Link{
+					HREF: "/lost",
+					Text: "Lost? Need help?",
+				},
+				&Link{
+					HREF: "https://twitter.com/marcusolsson",
+					Text: "@marcusolsson",
+				},
+			},
+		},
+    {
+			desc: "ex4.html",
+			name: "ex4.html",
+			want: []*Link{
+				&Link{HREF: "/dog-cat", Text: "dog cat"},
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			f, err := os.Open(tc.name)
+			if err != nil {
+				t.Fatalf("Parse() unable to read file %s. %v", tc.name, err)
+			}
+
+			got, err := Parse(f)
+			if err != nil {
+				t.Fatalf("Parse() unable to parse contents of %s. %v", tc.name, err)
+			}
+
+			if len(got) != len(tc.want) {
+				t.Fatalf("Parse()=%v, want=%v", got, tc.want)
+			}
+
+			for i := range got {
+				if *got[i] != *tc.want[i] {
+					t.Errorf("Parse() result %d got:%v, want:%v", i, got[i], tc.want[i])
+				}
+			}
+		})
+	}
+}
